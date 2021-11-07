d1af2d494d4241e80039bf582b0b48bde0690f0a
chenzhenfei <793385821@qq.com>
2021-11-07 22:54:43 +0800

    供应商索赔

---
+++ scripts/downloadIO.js
@@ -0,0 +1,31 @@
+const https = require('https')
+
+const data = JSON.stringify({
+  todo: '做点事情'
+})
+
+const options = {
+  hostname: 'nodejs.cn',
+  port: 8080,
+  path: '/todos',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length
+  }
+}
+
+const req = https.request(options, res => {
+  console.log(`状态码: ${res.statusCode}`)
+
+  res.on('data', d => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', error => {
+  console.error(error)
+})
+
+req.write(data)
+req.end()
