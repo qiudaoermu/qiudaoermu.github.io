0f68eddb6161d154657ddf1f4522a1e18970fe4b
qiudaoermu <793385821@qq.com>
2022-09-16 13:36:26 +0800

    ⬆ chore: 更换导航

--- _plugins/MyCustomProcessor.rb
+++ _plugins/MyCustomProcessor.rb
@@ -1,15 +1,15 @@
 class Jekyll::Converters::Markdown::MyCustomProcessor
   def initialize(config)
-    require 'markdown'
+    require 'redcarpet'
     @config = config
   rescue LoadError
     STDERR.puts 'You are missing a library required for Markdown. Please run:'
-    STDERR.puts '  $ [sudo] gem install markdown'
+    STDERR.puts '$ [sudo] gem install markdown'
     raise FatalException.new("Missing dependency: markdown")
   end
 
   def convert(content)
     puts '****************************************************************'
-    ::Markdown.new(content).to_html
+    ::Redcarpet::Render::HTML.new(render_options = {})
   end
 end
